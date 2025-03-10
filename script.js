e874bc635e1ea95f739186f387433727b9d1ee7f
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-10 21:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function mincost(arr)
 { 
+	var first=num1;
+	var second=num2;
+	for(let i=0;i<arr.length;i++){
+		if(arr[i]>first){
+			second=first;
+			first=arr[i];
+		}
+		else if(arr[i]>second && arr[i]<first){
+			second=arr[i];
+		}
+	}
+	return second;
 //write your code here
 // return the min cost
   
